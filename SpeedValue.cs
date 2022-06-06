d7bcacd2b700b77ad48b4065a59d133c4e8ceb8d
HoMka0IQ <71536727+HoMka0IQ@users.noreply.github.com>
2022-06-06 19:26:35 +0300

    Create SpeedValue.cs

---
+++ SpeedValue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedValue : MonoBehaviour
+{
+    public Vector3 pos, velocity;
+   
+    void Awake()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        velocity = (transform.position - pos) / Time.deltaTime;
+        pos = transform.position;
+       
+        
+    }
+}
