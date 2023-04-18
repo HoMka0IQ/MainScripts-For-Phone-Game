2fb7da26b3c786a2bf745abcfa06e258a5cdf601
HoMka0IQ <71536727+HoMka0IQ@users.noreply.github.com>
2023-04-18 12:09:16 +0300

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -6,76 +6,51 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] Transform playerCamera;
-
     [SerializeField] float WalkSpeed = 6f;
     [SerializeField] float gravity = -13.0f;
 
-
     [SerializeField] float verticalSpeed = -1f;
-
 
     [SerializeField] [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
     [SerializeField] [Range(0.0f, 0.5f)] float mouseSmoothTime = 0.3f;
 
     [SerializeField] bool lockCursor = true;
 
-
-
     float velocityY = 0.0f;
 
-    CharacterController controller = null;
-
-    [SerializeField] Camera mainCamera;
-
+    CharacterController controller;
 
     Vector2 currentDir = Vector2.zero;
     Vector2 currentDirVelocity = Vector2.zero;
 
     public VariableJoystick variableJoystick;
     
-
-    
     public Animator Animator;
     public SpeedValue SpeedValue;
 
     void Start()
     {
-       
-
         controller = GetComponent<CharacterController>();
-
-
     }
 
     void Update()
     {
-
-
         UpdateMovement();
 
-
-        if (SpeedValue.velocity.magnitude > 0.1f)
-        {
-            Animator.SetBool("Walk", true);
-        }
-        else
-        {
-            Animator.SetBool("Walk", false);
-        }
-        Animator.SetFloat("Speed", SpeedValue.velocity.magnitude / 15);
-
-
+        //if (SpeedValue.velocity.magnitude > 0.1f)
+        //{
+        //    Animator.SetBool("Walk", true);
+        //}
+        //else
+        //{
+        //    Animator.SetBool("Walk", false);
+        //}
+        //Animator.SetFloat("Speed", SpeedValue.velocity.magnitude / 15);
     }
 
-
-
-
-   
     void UpdateMovement()
     {
         Vector2 targenDir = new Vector2(variableJoystick.Direction.x, variableJoystick.Direction.y);
-
 
         currentDir = Vector2.SmoothDamp(currentDir, targenDir, ref currentDirVelocity, moveSmoothTime);
 
@@ -87,8 +62,5 @@ public class Movement : MonoBehaviour
         Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * WalkSpeed + Vector3.up * velocityY;
 
         controller.Move(velocity * Time.deltaTime);
-    
-
-
     }
 }
